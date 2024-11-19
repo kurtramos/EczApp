3f6ae9eecf2c064602ed73a99dd5f0fc009c189e
mccedddy <cedricmacallan@gmail.com>
2024-11-19 17:46:17 +0800

    Initialized i18n

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -16,8 +16,10 @@ import BottomNav from "../components/BottomNav";
 import { getAuth } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { firestore } from "../firebaseConfig";
+import { useTranslation } from "react-i18next";
 
 export default function HomeScreen() {
+  const { t, i18n } = useTranslation();
   const router = useRouter();
   const [userEmail, setUserEmail] = useState("");
   const [profileImage, setProfileImage] = useState("");
@@ -28,32 +30,36 @@ export default function HomeScreen() {
       const fetchUserData = async () => {
         const auth = getAuth();
         const user = auth.currentUser;
-  
+
         if (user) {
           setUserEmail(user.email ?? "Unknown Email");
           setProfileImage(user.photoURL || "https://via.placeholder.com/72x72");
-  
+
           try {
             const userDocRef = doc(firestore, "users", user.email ?? "");
             const userDocSnap = await getDoc(userDocRef);
-  
+
             if (userDocSnap.exists()) {
               const userData = userDocSnap.data();
               setFullName(`${userData.firstName} ${userData.lastName}`);
+              i18n.changeLanguage(userData.language);
             } else {
               Alert.alert("Error", "No user data found in Firestore.");
             }
           } catch (error) {
             console.error("Error fetching user data:", error);
-            Alert.alert("Error", "Failed to retrieve user data from Firestore.");
+            Alert.alert(
+              "Error",
+              "Failed to retrieve user data from Firestore."
+            );
           }
         }
       };
-  
+
       fetchUserData();
     }, [])
   );
-  
+
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
@@ -61,10 +67,11 @@ export default function HomeScreen() {
         <View style={styles.header}>
           <Image
             style={styles.profileImage}
-        source={require('../../assets/logos/EczemaCareLogoG.png')} 
+            source={require("../../assets/logos/EczemaCareLogoG.png")}
           />
           <View style={styles.greeting}>
-            <Text style={styles.welcomeText}>Hi, Welcome Back</Text>
+            {/* <Text style={styles.welcomeText}>Hi, Welcome Back</Text> */}
+            <Text style={styles.welcomeText}>{t("home.welcome")}</Text>
             <Text style={styles.welcomeText}>{fullName || "Guest"}</Text>
           </View>
           <View style={styles.iconContainer}>
@@ -83,13 +90,12 @@ export default function HomeScreen() {
           </View>
         </View>
 
-        
-
         {/* Header Title Section */}
         <View style={styles.titleCard}>
           <Text style={styles.headerTitle}>EczemaCare</Text>
           <Text style={styles.headerInfo}>
-          A Mobile Application for Monitoring and Tracking the Severity of Atopic Dermatitis.
+            A Mobile Application for Monitoring and Tracking the Severity of
+            Atopic Dermatitis.
           </Text>
         </View>
 
@@ -102,7 +108,10 @@ export default function HomeScreen() {
           <Text style={styles.infoText}>Everything to know about Eczema.</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.infoCard} onPress={() => router.push('/faqs')}>
+        <TouchableOpacity
+          style={styles.infoCard}
+          onPress={() => router.push("/faqs")}
+        >
           <Text style={styles.infoTitle}>HOW TO USE</Text>
           <Text style={styles.infoText}>
             How to use specific features and navigate the app.
@@ -120,7 +129,7 @@ export default function HomeScreen() {
         </TouchableOpacity>
       </ScrollView>
 
-      <BottomNav/>
+      <BottomNav />
     </View>
   );
 }
@@ -174,13 +183,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  
+
   Container: {
     flexDirection: "row",
     paddingHorizontal: 20,
     marginVertical: 20,
   },
-  
+
   titleCard: {
     backgroundColor: "#74BDB3",
     padding: 20,
@@ -204,7 +213,7 @@ const styles = StyleSheet.create({
     fontWeight: "400",
     marginBottom: 10,
   },
-  
+
   infoCard: {
     backgroundColor: "#C3EFE5",
     padding: 20,
